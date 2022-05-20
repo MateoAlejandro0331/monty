270a6eddde8c0601d0e130c84382b1eea3861b8d
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-05-19 21:50:36 -0500

    Pls work

--- pall.c
+++ pall.c
@@ -10,8 +10,6 @@ void _pall(stack_t **head, unsigned int line_number)
 	stack_t *current = NULL;
 	(void)(line_number);
 
-	if (!head)
-		exit(EXIT_FAILURE);
 	current = *head;
 	while (current != NULL)
 	{
