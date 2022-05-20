1fa586dc311efd100deaa6c0a3f56bb7c1e51d3b
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-05-20 14:37:31 -0500

    Adding pop function to delete de top node

--- monty.h
+++ monty.h
@@ -66,5 +66,6 @@ void (*get_op_func(char *s))(stack_t **stack, unsigned int line_number);
 int comparedigit(char *s, unsigned int line_number);
 void free_list(stack_t *head);
 void pint(stack_t **head, unsigned int line_number);
+void pop(stack_t **head, unsigned int line_number);
 
 #endif
