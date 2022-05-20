7c5ffc9788104629d2c34ccf4d830f77f63f76b2
xARKEINx <3884@holbertonschool.com>
2022-05-20 11:46:08 -0700

    swap

--- get_fun.c
+++ get_fun.c
@@ -11,6 +11,7 @@ void (*get_op_func(char *s))(stack_t **stack, unsigned int line_number)
 		{"push", _push},
 		{"pall", _pall},
 		{"pint", pint},
+		{"swap", swap},
 		{NULL, NULL}
 		};
 	int i = 0;
--- swap.c
+++ swap.c
@@ -1,7 +1,8 @@
 #include "monty.h"
 /**
- *
- *
+ * swap - swaps 2 elements of the stack
+ * @line_number: value of new node
+ * @head: double pointer to the first node
  */
 
 void swap(stack_t **head, unsigned int line_number)
@@ -10,13 +11,12 @@ void swap(stack_t **head, unsigned int line_number)
 	stack_t *tmp;
 
 	(void)(line_number);
-	if(!(*head) || !((*head)->next))
+	if (!(*head) || !((*head)->next))
 	{
-		fprintf(stderr,"L%u: can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
 		free_list(*head);
 		fclose(global.demo);
 		exit(EXIT_FAILURE);
-		
 	}
 	tmp = current->next;
 	current->prev = current->next;
